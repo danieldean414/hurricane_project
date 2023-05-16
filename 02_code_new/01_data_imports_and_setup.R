0b22aced08dbc08830acd0bb8c2b74cd845618f9
Daniel Dean <daniel.dean414@gmail.com>
2023-05-16 00:25:27 -0600

    Adding new, more-organzied code section so far; intend to add a 'summary' script for visuals/summary data, but 2 outstanding issues so far: 1) Some probably grep-related issues with assigning date/times to CLIMADA TC coordinates; using older pre-processed STORM data as a placeholder, but think it's solveable 2) As more of a hard barrier, R doesn't appear to be able to 'predict' from `modobj`, which I seem to take for granted in the code; trying to see if I can find any relevant data/files I missed

---
+++ 02_code_new/01_data_imports_and_setup.R
@@ -0,0 +1,205 @@
+########### libraries
+
+library(tidyverse)
+library(tigris)
+library(tidycensus)
+library(ggplot2)
+library(janitor)
+library(splines)
+library(drat)
+library(readxl)
+library(parallel)
+library(stormwindmodel)
+library(openxlsx)
+library(splines)
+library(plyr)
+library(hurricaneexposuredata)
+
+
+library(ncdf4)
+library(ncdf4.helpers)
+library(PCICt)
+library(tidyverse)
+
+########## Data
+
+############## Netcdf4 data (hurricane simulation output)
+
+# **  Using old data for now--issues w/ date-time agreement in CLIMADA output
+
+############# REPLACEW WITH EITHER CLIMADA OR CHAZ DATA ONCE DATES ARE RESOLVED
+
+
+#storm_25yr_sim_raw <- read_csv("../../Downloads/STORM_25_years.csv")
+storm_25yr_sim_raw <- read_csv("01_data/STORM_25_years.csv")
+# based on headers, already in knots this time
+
+storm_25yr_sim_3hr <- storm_25yr_sim_raw %>%
+  clean_names() %>%
+  mutate(year = year + 1,
+         #hours_base = timestep_3_hourly * 3,
+         hours_base = timestep_3_hourly * 3,
+         hour = hours_base %% 24,
+         day = ((hours_base - hour) / 24) + 1,
+         storm_id = paste(storm_number,
+                          str_pad(year, width = 4,
+                                  side = "left",
+                                  pad = 0),
+                          sep = "-")
+         #wind = wind * 1.94384449 # converting to knots
+  ) %>% 
+  mutate(date = 
+           paste0(str_pad(year, width = 4, side = "left", pad = 0),
+                  str_pad(month, width = 2, side = "left", pad = 0),
+                  str_pad(day, width = 2, side = "left", pad = 0),
+                  str_pad(hour, width = 2, side = "left", pad = 0),
+                  "00"
+                  
+           )) %>%
+  rename(
+    "latitude" = "lat",
+    "longitude" = "lon",
+    "wind" = "max_wind_kt"
+  ) %>%
+  select(storm_id, date, latitude, longitude, wind)
+
+
+
+#######
+
+######### County data <-- now that I'm using placeholder storm data, *could* combine at this stage
+      # once the date/time issue is corrected, maybe run that process first in a separate script?
+
+# counties (should default to 2020)
+
+us_counties <- tigris::counties(cb = TRUE, progress_bar = FALSE) %>% # To avoid high-res versions
+  clean_names()
+
+us_counties <- tidycensus::county_laea
+  # preloaded county geometry; hopefully doesn't cause any issues
+
+# Baseline mortality data for >=65 (should double check if there are any later versions)
+counties_mort_65 <- read_tsv("01_data/all_cause_mortality_65_plus_county.txt") %>%
+  dplyr::select(-Notes) %>%
+  clean_names()
+
+# might as well merge all county data this stage, I think
+
+# Setting up county variables for model
+  # original approach manually pulled from a downloaded Excel document; think I can get these with the tidycensus package
+    # maybe replace tigris should be the same county data
+
+  # some of these will have to come from TC and/or windfield models
+
+##  vmax_sust: modeled maximum sustained wind speed at the population centroid of the county during the TC in m/s (from hurricaneexposuredata package)
+##  sust_dur: duration of sustained wind speeds above 20 m/s at the population centroid of the county during the TC (from hurricaneexposuredata package)
+##  exposure: total number of TC exposures experienced by the county during 1999-2015 (derived from hurricaneexposuredata package)
+##  poverty_prop: proportion of residents in poverty          
+##  white_prop: proportion of residents identifying as white
+##  owner_occupied_prop: proportion of homes that are owner-occupied
+##  age_pct_65_plus_prop: proportion of residents age 65+
+##  median_age: county median age
+##  population_density: county population density
+##  median_house_value: county median house value
+##  no_grad_prop: proportion of residents without a high school diploma
+##  year: year the TC occurred
+##  coastal: whether the county is coastal (0/1)
+
+  # manually getting populations 65+
+    # oh, would be downstream, but may need to filter by minimum population
+
+# Sort of a ~'vestigial' step, but pulling in a table of ACS5 metadata 
+  # (vs looking up in `tidycensus`) for now
+  # not sure whether to stick with 2020 ACS5 data, or the dicennial census (different variable names)
+
+#tidycensus::load_variables(year = 2020, dataset = 'pl')
+  # not sure if I'm missing something, but looks like they don't have full data, so stick with ACS5?
+
+acs_vars <- read.xlsx("01_data/ACS2019_Table_Shells.xlsx")
+
+age_pct_65_plus_ids <- acs_vars %>%
+  clean_names() %>%
+  filter(table_id == "B01001" &
+           stub %in% c("65 and 66 years",
+                       "67 to 69 years",
+                       "70 to 74 years",
+                       "75 to 79 years",
+                       "80 to 84 years",
+                       "85 years and over") ) %>%
+  dplyr::select(-data_release)
+
+county_acs_vars <- tidycensus::get_acs(geography = 'county',
+                                       variables = c("B06012_001", 
+                                                     "B06012_002",
+                                                     "B02001_002",
+                                                     "B02001_001",
+                                                     "B07013_002",
+                                                     "B07013_001",
+                                                     age_pct_65_plus_ids$unique_id,
+                                                     "B01001_001",
+                                                     "B01002_001",
+                                                     "B25077_001",
+                                                     "B06009_002",
+                                                     "B06009_001"
+                                       ),
+                                       year = 2020,
+                                       survey = "acs5",
+                                       geometry = FALSE)
+coastlines <- read.xlsx("01_data/coastline-counties-list.xlsx", colNames = TRUE, startRow = 3) %>% clean_names()
+
+
+### Temporary (maybe load from a separate, earlier file)
+  # Oh, this is just the estimate for # exposures; so *could* keep from historical data
+
+exposure_draft1 <- hurricaneexposuredata::storm_winds %>% 
+  mutate(storm_year = str_extract_all(storm_id, "[0-9]{4}")) %>%
+  filter(storm_year %in% c(1999:2015) & vmax_sust > 20) %>%
+  select(fips, storm_id) %>%
+  unique() %>%
+  group_by(fips) %>%
+  dplyr::summarize(exposure = n())
+
+###
+
+county_acs_vars_bayesian <- county_acs_vars %>%
+  select(-moe) %>%
+  pivot_wider(names_from = variable,
+              values_from = estimate) %>%
+  mutate(poverty_prop = B06012_002 / B06012_001,
+         white_prop = B02001_002 / B02001_001,
+         owner_occupied_prop = B07013_002 / B07013_001,
+         age_pct_65_plus_prop = (B01001_020 +
+                                   B01001_021 +
+                                   B01001_022 +
+                                   B01001_023 +
+                                   B01001_024 +
+                                   B01001_025 +
+                                   B01001_044 +
+                                   B01001_045 +
+                                   B01001_046 +
+                                   B01001_047 +
+                                   B01001_048 +
+                                   B01001_049) / B01001_001,
+         median_age = B01002_001,
+         median_house_value = B25077_001,
+         no_grad_prop = B06009_002 / B06009_001
+  ) %>%
+  left_join(clean_names(coastlines), by = c("GEOID" = "state_county_fips")) %>%
+  left_join(us_counties, by = c("GEOID" = "geoid")) %>%
+  mutate(population_density = B01001_001 / (aland * 0.00000038610), 
+         coastal = as.numeric(!is.na(coastline_region)),
+         year = 2015,
+         #year = sample(2006:2015, size = 1, replace = T),
+         #exposure = median(modobj$data$exposure)) %>%
+  ) %>%
+  left_join(exposure_draft1, by = c("GEOID" = "fips")) %>%
+  select(GEOID, poverty_prop:no_grad_prop, coastal:exposure, population_density, median_house_value)
+
+# last 3 lines are pulling in storm simulation data; I guess could run that 'upstream'
+
+# model object
+
+load("01_data/modobj.RData")
+
+
+
---
+++ 02_code_new/03_health_impact_bayesian.R
@@ -0,0 +1,149 @@
+
+# trying to work out which of these are ~functional/final steps
+#################################################################33
+
+# Using historical data
+test_bayesian_data <- (hurricaneexposuredata::storm_winds %>% 
+                         left_join(county_acs_vars_bayesian, 
+                                   by = c("fips" = "GEOID")))
+
+test_bayesian <- predict(modobj, newdata = (test_bayesian_data %>% filter(!is.na(median_house_value))))
+
+####
+
+#test_bayesian_data_conus <- (amo_merged_250_clean_get_grid %>% 
+#                               left_join(county_acs_vars_bayesian, 
+#                                         by = c("gridid" = "GEOID")))
+
+test_bayesian_data_conus <- tc_merged_data
+
+test_bayesian_conus <- predict(modobj, newdata = (test_bayesian_data_conus %>% 
+                                                    filter(!is.na(median_house_value))))
+
+# OK, keep running into error 
+  # Error in UseMethod("predict") : 
+      # no applicable method for 'predict' applied to an object of class "tcExcessMort"
+
+# Don't seem to have at least made any comments on this, so maybe there's some auxiliary function I'm missing?
+
+#### OK, filtering for rep dropped RAM load, but still too large; let's try a loop and/or parallelization?
+# If so, maybe try filtering by county? (Trying to anticipate ~nonlinear responses)
+
+# Processing for setup:
+
+test_bayesian_data_conus_split <- split(test_bayesian_data_conus,
+                                        f = paste(test_bayesian_data_conus$storm_id,
+                                                  test_bayesian_data_conus$scenario,
+                                                  sep = "_"))
+
+
+test_bayesian_conus_list <- list()
+
+for(storm_id_TMP in test_bayesian_data_conus_split){
+  test_bayesian_data_conus_TMP <- storm_id_TMP %>% filter(!is.na(median_house_value))
+  test_bayesian_conus_TMP <- predict(modobj, newdata = test_bayesian_data_conus_TMP) %>%
+    data.frame()
+  test_bayesian_conus_list <- append(test_bayesian_conus_list, test_bayesian_conus_TMP)
+}
+
+
+
+#### Nope--still not enough memory (same figure of 28.7 Gb as well); let's try parallel processing?
+
+# parallel processing
+
+# It isn't handling "external" objects well
+
+# Setting up function?
+
+modobj_predict <- function(x){
+  A <- predict(modobj, newdata = x)
+  return(A)
+}
+
+# Another method?
+
+result_list <- llply(test_bayesian_data_conus_split, function(x) {
+  # do the replacing
+  return(x)
+})
+
+
+library(parallel)
+#all
+start <- proc.time()
+#cl <- makeCluster(7, "modobj") # 8 for ~full utilization
+cl <- makeCluster(5) # 8 for ~full utilization
+clusterEvalQ(cl, source('02_code/pred_function.R'))
+#clusterEvalQ(cl, library(Hmisc))
+test_bayesian_data_conus_split_par <- parLapply(cl,
+                                                test_bayesian_data_conus_split,
+                                                modobj_predict
+)
+stopCluster(cl)
+end <- proc.time()
+print(end - start) 
+
+
+
+# trying to figure out how it lines up with FIPS; seems to drop rows w/ missing data
+test_bayesian_10 <- predict(modobj, newdata = test_bayesian_data[1:10,])
+# So I think every row is a county (w/ 1000 predictions?); I have to assume in order provided
+# However, full dataset is shorter than provided list; presumably dropping relevant NAs
+# could create ~cascading mismatches since it jsut outputs a numeric dataframe
+# so need to prefilter carefully (trying that above--let's see if it matches)
+
+# OK, removing the 342 counties without median house value data accounts for difference in length
+
+
+test_bayesian_fixed_year <- predict(modobj, newdata = (test_bayesian_data %>% mutate(year = 2015)))
+
+county_mortality_pred_bayesian <- function(fips = counties_mort_65$`County Code`,
+                                           storm_data = (hurricaneexposuredata::storm_winds %>% 
+                                                           left_join(county_acs_vars_bayesian, 
+                                                                     by = c("fips" = "GEOID"))),
+                                           pred_model = modobj,
+                                           counties = counties_mort_65,
+                                           windspeed_min = 0,
+                                           windspeed_max = Inf,
+                                           storm_ids = ".*") {
+  estimates <- counties %>%
+    clean_names() %>%
+    filter(county_code %in% fips) %>%
+    inner_join(
+      storm_data %>%
+        filter(vmax_sust > windspeed_min &
+                 vmax_sust <= windspeed_max) %>%
+        filter(str_detect(storm_id, storm_ids)) %>% 
+        mutate(storm_year = as.numeric(
+          str_extract(storm_id, "[0-9]{4}"))) %>%
+        mutate(storm_years = length(unique(storm_year))),
+      mutate(
+        storm_years = max(storm_year - min(storm_year)) + 1
+      ),
+      by = c("county_code" = "fips")
+    )
+  
+  if(nrow(estimates > 0)){
+    estimates <- estimates %>%
+      mutate(est_excess_death_rate = 
+               #predict(pred_model, newdata = data.frame( windspeed = vmax_sust)),
+               predict(pred_model, newdata = storm_data),
+             est_excess_deaths = (est_excess_death_rate *  population) / 100000)
+    return(estimates %>%
+             select(county_code, state, county, vmax_sust, storm_id, storm_year, est_excess_death_rate, est_excess_deaths, population, deaths, crude_rate, storm_years))
+  }
+  
+  else{
+    return("No data meeting parameters")
+  }
+  
+}
+
+county_mortality_pred_bayesian(pred_model = predict.tcExcessMort)
+
+
+
+predict.tcExcessMort(newdata = (hurricaneexposuredata::storm_winds %>% 
+                                  left_join(county_acs_vars_bayesian, 
+                                            by = c("fips" = "GEOID"))))
